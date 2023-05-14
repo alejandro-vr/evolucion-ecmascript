c18883ca6dc0d88d886774f9ba6014d98bfdf93c
Alejandro Valencia <alejandrovalenciarendon@gmail.com>
2023-05-13 22:13:39 -0500

    Se revisó el operador de propagación '...' que permite agregar nuevas propiedades (keys), con sus respectivos valores, a un objeto mientras de desestructura

--- src/es6/04-res-spred.js
+++ src/es6/04-res-spred.js
@@ -1,11 +1,18 @@
-// Arrays destructuring
+// Arrays destructuring "let [a, b] = fruits;"
 let fruits = ['Apple', 'Banana'];
 let [a, b] = fruits;
 console.log(a, b);
 console.log(a, fruits[1]);
 
-// Object destructuring
+// Object destructuring "let { username, age } = user;"
 let user = { username: 'Oscar', age: 34 };
 let { username, age } = user;
 console.log(username, age);
 console.log(username, user.age);
+
+// spread operator "...person"
+let person = { name: 'Maria', age: 21 };
+let country = 'CO';
+
+let data = { id: 1, ...person, country};
+console.log(data);
