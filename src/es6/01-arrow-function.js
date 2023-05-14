b79fc2dfeab6b28c3fe19fde868ec6639cc14f9d
Alejandro Valencia <alejandrovalenciarendon@gmail.com>
2023-05-13 21:35:13 -0500

    Se revisó la nueva sintaxis tipo 'arrow' para la funciones en la version es6

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,12 @@
+function square1(num) {
+    return num * num;
+}
+console.log(square1(2));
+
+const square2 = (num) => {
+    return num * num;
+}
+console.log(square2(3));
+
+const square3 = num => num * num;
+console.log(square3(4));
