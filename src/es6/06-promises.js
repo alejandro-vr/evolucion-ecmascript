8b40b139c18451fe49de313c7bc0e4ae2dd5e5c7
Alejandro Valencia <alejandrovalenciarendon@gmail.com>
2023-05-13 23:06:49 -0500

    Se revisó a poca profundidad el concepto de 'promesas' para trabajar con asincronismos

---
+++ src/es6/06-promises.js
@@ -0,0 +1,14 @@
+// Promises for async
+const anotherFunction = () => {
+  return new Promise((resolve, reject) => {
+    if(false) {
+      resolve('Hey!');
+    } else {
+      reject('Wooooops!');
+    }
+  });
+}
+
+anotherFunction()
+  .then(response => console.log(response))
+  .catch(err => console.log(err));
