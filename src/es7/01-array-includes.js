90137e5d1b3b11b579975a31b7571e030dead8d0
Alejandro Valencia <alejandrovalenciarendon@gmail.com>
2023-05-22 13:23:00 -0500

    Se revisaron los cambios agregados en la versión ecma script 7. Primero, el operador '**' para calcular potencias de manera directa sin tener que recurrir a le metodo '.pow()' de la biblioteca 'Math'. Segundo, el metodo 'include()' de los arrays que sirve para verificar si un elemento se encuentra incluido dentro del array

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,9 @@
+let numbers = [1, 3, 4, 6, 7, 8];
+
+console.log(numbers.includes(4));
+console.log(numbers.includes(9));
+
+let names = ['Oscar', 'David', 'Ana'];
+
+console.log(names.includes('Oscar'));
+console.log(names.includes('oscar'));
