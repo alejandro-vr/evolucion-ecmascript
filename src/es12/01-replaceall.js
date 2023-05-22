8398cd7f93dc81f3bf0cb9e6da9763a4c7c135be
Alejandro Valencia <alejandrovalenciarendon@gmail.com>
2023-05-22 18:40:46 -0500

    Se revisó el nuevo metodo 'replaceAll' agregado a los strings, para reemplazar un subestring1 con otro subestring2 especifico, todas las veces que el substring1 aparezca dentro del string

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,7 @@
+// Method 'replaceAll()' in strings
+const string = "Java es un maravilloso lenguaje, Java sirve para todo!";
+
+const replaceString = string.replaceAll("Java", "JavaScript");
+
+console.log(string);
+console.log(replaceString);
