72dea40d0970ccdb9c0b22fcf205fc1d73b19cd3
Alejandro Valencia <alejandrovalenciarendon@gmail.com>
2023-05-22 17:08:17 -0500

    Se revisó el método 'BigInt()' para crear enteros muy grandes

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,5 @@
+// BigInt for too big numbers
+const aBigNumber = 198512384912030009381n;
+const anotherBigNumber = BigInt(298512384912030009382);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
